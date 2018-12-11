418c83b9cd7870bad4b69b9454f4c9a0613f4089
Mitch Pierias <mitch@pierias.com>
2018-12-11 12:46:40 +1000

    Refactored TaskListItem
    - Refactored TaskListItem into a dumb component and abstracted logic up too the parent

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -1,9 +1,9 @@
 // Modules
 import React from 'react';
+import _ from 'lodash';
 import { string, number } from 'prop-types';
 // Components
 import TaskListItemState from './TaskListItemState';
-import TaskListItemDelete from './TaskListItemDelete';
 // Formats
 const VIDEO_FORMATS = [
   {value:'avi',label:'AVI'},
@@ -18,74 +18,58 @@ import '../styles/TaskListItem.css';
 
 type VideoFormats = 'avi' | 'm4v' | 'mov' | 'mp4' | 'mpeg' | 'ogv';
 
-type Props = {
-	onChange(hash: string, values: object): void;
-	didSelectShow(hash: string): void;
-	didSelectRemove(hash: string): void;
-} & Partial<DefaultProps>
-
-type DefaultProps = ReadOnly<typeof defaultProps>
-
-const defaultProps = {
-	hash: string,
-	name: string,
-	label: string,
-	format: 'avi' | 'm4v' | 'mov' | 'mp4' | 'mpeg' | 'ogv',
-	output: 'avi' | 'm4v' | 'mov' | 'mp4' | 'mpeg' | 'ogv',
-	progress: number
+type PropTypes = {
+	id: string;
+	name: string;
+	title: string;
+	format: VideoFormats;
+	outputs: array<VideoFormats>;
+	onChange(id: string, values: object): void;
+	onShow(id: string): void;
+	onReset(id: string): void;
+	onRemove(id: string): void;
 }
 
-export default class TaskListItem extends React.Component<Props> {
+export default function TaskListItem(props: PropTypes) {
 
-	static defaultProps = defaultProps;
+	const {
+		id,
+		title,
+		name,
+		format,
+		outputs,
+		onReset,
+		onRemove,
+		onChange,
+		onShow
+	} = props;
 
-	didSelectShow(event) {
-		event.preventDefault();
-		const { directory, name, format, output } = this.props;
-		const directoryPath = directory + name.replace(format, output);
-		this.props.onShow(directoryPath);
-	}
+	const progress = (outputs.length === 0) ? 0 : outputs.reduce((sum, output) => sum+(output.progress||0)) / outputs.length;
 
-	didChangeOutput(event) {
-		event.preventDefault();
-		const outputFormat = event.target.value.toLowerCase();
-		this.props.onChange(this.props.hash, {output:outputFormat});
-	}
-
-	didSelectRemove(event) {
-		event.preventDefault();
-		this.props.onRemove(this.props.hash);
-	}
-
-	render() {
-
-		const {
-			label,
-			name,
-			format,
-			output,
-			progress
-		} = this.props;
-
-		return (
-			<div className="task-list-item" data-js="TaskListItem" style={{...this.props.style}}>
-				<TaskListItemState progress={progress} complete={progress===100} range={[0,30]}/>
-				<span className="task-label">
-					<input className="name" type="text" defaultValue={label}/>
-					<span className="task-title">
-						<label className="">{format.toUpperCase()}</label>
-						<span className="name">{name}</span>
-					</span>
+	return (
+		<div className="task-list-item" data-js="TaskListItem" style={{...props.style}}>
+			<TaskListItemState progress={progress} complete={progress===100} range={[0,30]}/>
+			<span className="task-label">
+				<input className="name" type="text" defaultValue={title}/>
+				<span className="task-title">
+					<label className="">{format.toUpperCase()}</label>
+					<span className="name">{name}</span>
 				</span>
-				<span className="format-container">
-					<select className="format" defaultValue={output} onChange={this.didChangeOutput.bind(this)}>
-						{VIDEO_FORMATS.map(FORMAT => {
-							if (FORMAT.value !== format) return <option key={FORMAT.value} value={FORMAT.value}>{FORMAT.label}</option>;
-						})}
-					</select>
-					<button onClick={this.didSelectRemove.bind(this)}>remove</button>
-				</span>
-			</div>
-		)
-	}
+			</span>
+			<span className="format-container">
+				{outputs.map((output, idx) => 
+					<span key={idx} className="format"><span>{output.format}</span><button className="format-control" onClick={event => onChange(id, {output:event.target.value.toLowerCase()})} value={output.format}>X</button></span>
+				)}
+				<select className="format" defaultValue={false} placeholder="select" onChange={event => onChange(id, {output:event.target.value.toLowerCase()})}>
+					{VIDEO_FORMATS.map(FORMAT => {
+						if (FORMAT.value !== format && _.findIndex(outputs,['format',FORMAT.value]) < 0) return <option key={FORMAT.value} value={FORMAT.value}>{FORMAT.label}</option>;
+					})}
+				</select>
+				{(outputs.length > 0)
+					? <button onClick={event => onReset(id)}>reset</button>
+					: <button onClick={event => onRemove(id)}>remove</button>
+				}
+			</span>
+		</div>
+	)
 }
