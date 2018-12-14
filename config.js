1286e76060c325783bd584cf0794b7beda27cc35
Mitch Pierias <mitch@pierias.com>
2018-12-15 09:55:45 +1000

    Config file

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const ffmpegPath = require('ffmpeg-static').path;
+const ffprobePath = require('ffprobe-static').path;
 
 module.exports = {
-	ffmpegPath: ffmpegPath
+	ffmpegPath: ffmpegPath,
+	ffprobePath: ffprobePath
 }
