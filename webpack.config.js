0a087b779d2c1c5a3e7e85acc2161de1c3d99ea6
Mitch Pierias <mitch@pierias.com>
2018-12-15 09:54:23 +1000

    Webpack HTML & CSS loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,89 @@
 const webpack = require('webpack');
 const path = require('path');
 const fs = require('fs');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
+/**
+ * HTML Webpack Plugin
+ * @desc Configuration for building the HTML page
+ * @note Some props are injected and some are configuration (rendering) settings
+ */
+const htmlPlugin = new HtmlWebPackPlugin({
+	title: "Agora",
+	template: "./src/index.html",
+	filename: "./index.html",
+	favicon: "./assets/icon.ico",
+	meta: {
+		"viewport": "width=device-width, initial-scale=1, shrink-to-fit=no",
+		"theme-color": "#f9f9f9"
+	},
+	minify: {
+		collapseWhitespace: true,
+		removeComments: true,
+		removeRedundantAttributes: true,
+		removeScriptTypeAttributes: true,
+		removeStyleLinkTypeAttributes: true,
+		useShortDoctype: true
+	}
+});
+
+/**
+ * External Node Module Inclusion
+ * @desc Inject commonjs supported Node modules into our React application
+ */
 const nodeModules = {};
-
-// Attach Node modules into our React application
 fs.readdirSync('node_modules').filter(function(x) {
 	return ['.bin'].indexOf(x) === -1;
 }).forEach(function(mod) {
 	nodeModules[mod] = 'commonjs ' + mod;
 });
 
+/**
+ * Webpack Configuration
+ */
 module.exports = {
 	externals: nodeModules,
-	entry: [
-		'./src/App.js'
-	],
-	target: 'node',
+	entry: './src/App.js',
+	target: 'electron-renderer',
 	output: {
-		path: __dirname,
-		publicPath: '/public',
+		path: path.resolve(__dirname, 'build'),
+		publicPath: './',
 		filename: 'bundle.js'
 	},
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
 				exclude: [
 					/node_modules/,
 					/.json?/
 				],
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015', 'stage-1']
+				use: {
+					loader: 'babel-loader'
 				}
 			}, {
 				test: /\.(s*)css$/,
-				use: ['style-loader','css-loader','sass-loader']
+				use: [
+					{
+						loader:'style-loader'
+					}, {
+						loader:'css-loader'
+					}, {
+						loader:'sass-loader'
+					}
+				]
 			}
 		]
 	},
+	plugins:[
+		htmlPlugin
+	],
 	resolve: {
 		extensions: ['.js','.jsx','.ts','.tsx']
 	},
 	devServer: {
 		historyApiFallback: true,
-		contentBase: './',
+		contentBase: path.join(__dirname, 'build'),
 		port: 4172
 	}
-};
+}
