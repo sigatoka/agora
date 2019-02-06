4f3efdbe68d5415c6a0d92c0ec59a4dfcb3974b7
Mitch Pierias <mitch@pierias.com>
2019-02-06 12:38:04 +1000

    Added conversion service and tests

---
+++ test/convert.test.js
@@ -0,0 +1,37 @@
+const { assert } = require('chai');
+const { convertFile } = require('./../service/convert');
+const fs = require('fs');
+const path = require('path');
+// Configuration
+const SAMPLE_FILE = 'snippet_360p.mp4';
+const OUTPUT_FORMAT = 'avi';
+const SAMPLE_FILE_DIR = path.join(__dirname, 'samples');
+const SAMPLE_FILE_PATH = path.join(SAMPLE_FILE_DIR, SAMPLE_FILE);
+
+describe("Convert", () => {
+
+    before(cleanup);
+    
+    it("Should convert file", () => {
+        return convertFile(SAMPLE_FILE_PATH, OUTPUT_FORMAT, (progress) => {
+            assert.isAbove(progress, 0, 'Progress should be greater than zero');
+            assert.isBelow(progress, 1, 'Progress should be less than one');
+        }).then(outputPath => {
+            assert.isTrue(fs.existsSync(outputPath), `File '${outputPath}' should exist`);
+        }).catch(console.log);
+    }).timeout(20000);
+
+    after(cleanup);
+});
+
+function cleanup(callback) {
+    // Get all filenames within sample directory
+    fs.readdir(path.join(__dirname, 'samples'), (err, files) => {
+        // Iterate and remove additional files
+        files.forEach(filename => {
+            if (filename === SAMPLE_FILE) return;
+            fs.unlinkSync(path.join(SAMPLE_FILE_DIR, filename));
+        });
+        callback();
+    });
+}
